7d276d3232987afa330c12ec7460025059108e63
MaxFangX <maxfangx@gmail.com>
2014-10-25 23:31:02 -0700

    First working version

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
 
-$('.carousel').carousel()
+$(window).scroll(function(e){
+    parallax();
+});
+
+//$('.carousel').carousel()
