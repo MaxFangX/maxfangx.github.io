f29b1e643cab218dc69a8123df79516f86f0eb3d
MaxFangX <maxfangx@gmail.com>
2014-11-14 21:07:22 -0800

    Change slides with test buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+$('.test-button1').click(function(){
+	alert('this button fucking works');
+	$('.main-display').style.background = url('../img/marimba_full.jpg');
+});
+$('.test-button2').click(function(){
+	$('.main-display').style.background = url('../img/darkcoin_onyx.png');
+});
+$('document').click(function(){
+	alert('lol ok');
+})
+
+
+
 // var jumboHeight = $('.jumbotron').outerHeight();
 // function parallax(){
 //     var scrolled = $(window).scrollTop();
