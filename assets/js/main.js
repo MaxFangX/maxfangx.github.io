cc59e498bce919337b5cba14692128bfaa27b68c
MaxFangX <maxfangx@gmail.com>
2014-12-02 14:58:08 -0800

    BIG! Compacted all functionality into one listener

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,31 @@
-var slides = null;
-//Carousel buttons
-$('.indicator-1').click(function(){
-	$('span.x').html("X.");
-	$('.main-display').css("background", 
-		$('#slide-1')[0].dataset.background);
-});
-$('.indicator-2').click(function(){
-	$('span.x').html("Bitcoiner.");
-	$('.main-display').css("background", 
-		$('#slide-2')[0].dataset.background);
-});
-$('.indicator-3').click(function(){
-	$('span.x').html("Foo bar.");
-	$('.main-display').css("background", 
-		$('#slide-3')[0].dataset.background);
-});
-
-//Update .main-display css when slide changes
-//All CSS for .main-display background needs to be copied here
-$('.slide-changer').click(function(){
+$('.slide').click(function(){
+	$('span.x').html(this.dataset.word);
+	console.log(this.dataset.background);
+	$('.main-display').css("background", this.dataset.background);
+	var jprev = $('.current');
+	var jthis = $(this);
+	console.log(jprev);
+	console.log(jthis);
+	console.log(jprev.get(0));
+	console.log(jthis.get(0));
+	if(jprev[0] == jthis[0]){
+		//Do nothing
+	}
+	else{
+		jprev.removeClass('current'); //Switch current class
+		jthis.addClass('current');
+		jprev.removeClass('fa-dot-circle-o'); //Switch icon
+		jprev.addClass('fa-circle-o');
+		jthis.removeClass('fa-circle-o');
+		jthis.addClass('fa-dot-circle-o');
+	}
+	//Update .main-display css when slide changes
+	//Should be copied anywhere slide changes
 	$('.main-display').css("background-size", "contain");
 	$('.main-display').css("background-repeat", "no-repeat");
 })
 
 //On page load
 $(document).ready(function(){
-
-	//Set the id attributes dynamically
-	slides = $('.slide');
-	for(var i = 0, slide; slide = slides[i]; i++){
-		//Following two lines are equivalent
-		//slides[i].setAttribute('data-slide-id', i);
-		//slides[i].dataset.slideId = i;
-		slides[i].setAttribute('id', "slide-"+(i+1)); //Start index 1
-		//alert(slides[i].dataset.slideId);
-	}
 })
 
-// var jumboHeight = $('.jumbotron').outerHeight();
-// function parallax(){
-//     var scrolled = $(window).scrollTop();
-//     $('.bg').css('height', (jumboHeight-scrolled) + 'px');
-// }
-
-// $(window).scroll(function(e){
-//     parallax();
-// });
-
-//$('.carousel').carousel()
